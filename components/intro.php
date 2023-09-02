abdd1254233a15f78b16d5f31923416109594761
Matheus Mariano <matheusgameplay9@gmail.com>
2023-09-02 00:47:20 -0300

    v001 - first release.
    first release of course, the prototype of a big business project.

---
+++ components/intro.php
@@ -0,0 +1,13 @@
+<section class="intro-background">
+
+        <img src="./views/images/background-one.jpg" alt="Construtores em serviço.">
+
+    <section class="intro-content">
+
+        <h1>Construa com a gente.</h1>
+
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec convallis sapien vitae gravida tristique. Fusce scelerisque imperdiet nunc ut imperdiet... :)</h3>
+
+    </section>
+
+</section>
---
+++ components/navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar">
+
+    <section class="logo-image">
+        <a href="#"><img src="./views/images/main_logo.png" alt="ETC Logo."></a>
+    </section>
+
+    <section class="navbar-links">
+
+        <ul>
+
+            <li><a href="#">Serviços</a></li>
+            <li><a href="#">Segurança</a></li>
+            <li><a href="#">Suporte</a></li>
+        
+        </ul>
+
+        <section class="navbar-login">
+            <button class="button-secondary shadow"> Entrar </button>
+        </section>
+
+    </section>
+
+</nav>
