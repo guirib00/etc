681c863046665cdee2ec692fcc0f120effe570a7
guirib00 <guilhermerib2013@hotmail.com>
2023-11-21 16:27:16 -0300

    Atualizando o botão do contratante.

--- components/navbar.php
+++ components/navbar.php
@@ -29,7 +29,7 @@ var_dump($userType);
         </ul>
 
         <section class="navbar-login">
-            <?php if ($loggedIn == false) { ?> 
+            <?php if ($loggedIn == false) { ?>
                 <a href="painel-login.php">
                     <button class="button button-secondary shadow"> Entrar </button>
                 </a>
@@ -40,18 +40,34 @@ var_dump($userType);
                     <!-- Botão de perfil para trabalhador -->
                     <a href="profile.php?id=<?php echo $_SESSION['account_id']; ?>">
                         <button class="button button-secondary button-perfil shadow" style="padding: 0.5rem 2rem;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/></svg>
                         </button>
                     </a>
                 <?php } elseif ($userType == 'contratante') { ?>
                     <!-- Botão de perfil para contratante -->
-                    <a href="contr.php?id=<?php echo $_SESSION['account_id']; ?>">
-                        <button class="button button-secondary button-perfil shadow" style="padding: 0.5rem 2rem;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/></svg>
+                    <div class="dropdown" style="position: relative;">
+                        <button onclick="showContractorProfile()" class="button button-secondary button-perfil shadow" style="padding: 0.5rem 2rem;">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/></svg>
                         </button>
-                    </a>
+                        <div id="contractorProfile" class="dropdown-content text-center" style="display: none; position: absolute; background-color: #f9f9f9; min-width: 120px; box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2); z-index: 1;">
+                            <p id="contractorName" style="margin: 5px;"><?php echo $username; ?></p>
+                            <a id="logoutLink" href="./modules/logout.php">Sair</a>
+                        </div>
+                    </div>
                 <?php } ?>
             <?php } ?>
         </section>
     </section>
 </nav>
+
+<script>
+    function showContractorProfile() {
+        var profileDropdown = document.getElementById("contractorProfile");
+
+        if (profileDropdown.style.display === "none") {
+            profileDropdown.style.display = "block";
+        } else {
+            profileDropdown.style.display = "none";
+        }
+    }
+</script>
